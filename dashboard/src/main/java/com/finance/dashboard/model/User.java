20c0264d2167ddba1ca2bb763cb55c886f8b7b85
kshitijrat <kshitijratnawat@gmail.com>
2026-04-01 20:45:38 +0530

    create User Model

---
+++ dashboard/src/main/java/com/finance/dashboard/model/User.java
@@ -0,0 +1,33 @@
+package com.finance.dashboard.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.finance.dashboard.enums.Role;
+
+import lombok.*;
+
+
+
+@Document(collection = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+    private Role role;
+
+    private boolean isActive;
+}
