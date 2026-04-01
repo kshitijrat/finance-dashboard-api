aca6d259a3ca98c1c3cff9df346cd945047412e0
kshitijrat <kshitijratnawat@gmail.com>
2026-04-01 20:54:39 +0530

    create Dto Request

---
+++ dashboard/src/main/java/com/finance/dashboard/dto/request/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.finance.dashboard.dto.request;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    private String email;
+    private String password;
+}
---
+++ dashboard/src/main/java/com/finance/dashboard/dto/request/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.finance.dashboard.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    private String name;
+    private String email;
+    private String password;
+}
