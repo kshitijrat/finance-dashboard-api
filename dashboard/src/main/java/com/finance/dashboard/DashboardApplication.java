46d1bd18f1cd07db4be1cfc2f2e8adaac4f19ea4
kshitijrat <kshitijratnawat@gmail.com>
2026-04-01 21:14:47 +0530

    fix issue Datab Not Found

--- dashboard/src/main/java/com/finance/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/com/finance/dashboard/DashboardApplication.java
@@ -8,6 +8,7 @@ public class DashboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardApplication.class, args);
+		System.out.println("Create Application....");
 	}
 
 }
---
+++ dashboard/src/main/java/com/finance/dashboard/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.finance.dashboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.finance.dashboard.security.JwtAuthFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
